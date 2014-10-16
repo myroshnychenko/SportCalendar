642c701e9254f4ed59c67afa77b7fad87921333c
d.myroshnychenko <d.myroshnychenko@levi9.com>
2014-10-16 21:26:47 +0300

    Init commit

---
+++ app/cache/prod/twig/d6/9b/2be1c62a3ab020a882c8d2598d478b11fd59697b32fd320ffc5452c9bf68.php
@@ -0,0 +1,39 @@
+<?php
+
+/* FirstBundle:Default:index.html.twig */
+class __TwigTemplate_d69b2be1c62a3ab020a882c8d2598d478b11fd59697b32fd320ffc5452c9bf68 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : null), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "FirstBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/WSP/FirstBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WSP\FirstBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FirstBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
